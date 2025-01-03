1176cbd48b232098bbc358e28ad0f26dd5537797
SehnaazIbrahim <159917934+SehnaazIbrahim@users.noreply.github.com>
2025-01-03 19:56:47 +0530

    Create Jan_3.java

---
+++ Daily_Problems/January/Jan_3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long[] prefix = new long[nums.length];
+
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+
+        int count=0;
+
+        for(int i=0;i<nums.length-1;i++){
+
+            if(prefix[i]>=prefix[nums.length-1]-prefix[i]){
+                count++;
+            }
+
+        }
+        return count;
+
+
+    }
+}
